a9887bc0e1808d12557bb9bc74ff00ef6ce7e76a
GiuseppeDalessio7 <g.dalessio03@gmail.com>
2023-10-10 16:59:26 +0200

    Try to bonus

--- index.php
+++ index.php
@@ -56,11 +56,10 @@ foreach ($hotels as $allhotel) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hotel</title>
-    <style>
-    </style>
+    <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-    <link rel="stylesheet" href="style.css">
+
 </head>
 
 <body>
@@ -68,7 +67,23 @@ foreach ($hotels as $allhotel) {
 
     <h1 class="text-center">Lista degli Hotel</h1>
 
-    <table class="table bg-dark">
+
+    <form class="my-4 text-center" action="" method="GET">
+        <input type="checkbox" name="hotelparking" id="hotelparking"> <span class=" bold">Parcheggio</span>
+        <button type="submit" class="btn btn-primary"> Cerca </button>
+
+        <?php
+        if (($_GET[$hotel["parking"]] === true)) {
+            echo 'Gli hotel con il parcheggio sono:' . $_GET[$hotel["parking"]];
+        } else {
+            $_GET[$hotel["parking"]] === false;
+            echo "non ci sono parcheggi";
+        }
+        ?>
+    </form>
+
+
+    <table class="table">
         <tbody>
             <tr>
                 <th class="text-danger" scope="row">Name</th>
